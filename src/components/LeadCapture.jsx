ec9b399a6f9631bfa68a4edf20bce9c454ae8ec3
Liz Kline <lizkline91@gmail.com>
2016-09-27 11:19:35 -0400

    added some refs to access child components in validation form

--- src/components/LeadCapture.jsx
+++ src/components/LeadCapture.jsx
@@ -1,12 +1,29 @@
 var React = require('react');
+var EmailField = require('./EmailField.jsx');
+var NameField = require('./NameField.jsx');
 
 var LeadCapture = React.createClass({
+  onSubmit: function(e) {
+    if (!this.refs.fieldEmail.state.valid) {
+      alert("Not valid form. Email required!")
+    } else {
+      var httpRequiestBody = {
+        email: this.refs.fieldEmail.state.value,
+        firstName: this.refs.fieldName.state.value
+      };
+      this.refs.fieldEmail.clear();
+      this.refs.fieldName.clear();
+    }
+  },
   render: function() {
     return(
       <div className="call-sm-3">
         <div className="panel panel-default">
           <div className="panel-body">
-            
+            <NameField type="First" ref="fieldName"/>
+            <br />
+            <EmailField ref="fieldEmail"/>
+            <button className="btn btn-primary" onClick={this.onSubmit}>Submit</button>
           </div>
         </div>
       </div>
